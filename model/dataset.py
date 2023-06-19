8c04802088fd830a56a093477b3c2540cf457270
Junyoung Shin <jun021222@naver.com>
2023-06-19 05:35:26 +0000

    Modify dataset.py

--- model/dataset.py
+++ model/dataset.py
@@ -16,19 +16,25 @@ def _transform(x):
     return repeat_data(x.transpose())
 
 class HSEDataset(Dataset):
-    def __init__(self, data_path, transform=_transform):
+    def __init__(self, data_path, transform=_transform, predict_only=False):
         self.frontal = []
         self.lateral = []
         self.shape = []
         self.names = []
 
         self.transform = transform
+        self.predict_only = predict_only
 
         for dir in tqdm(os.listdir(data_path), desc='loading data'):
             path = os.path.join(data_path, dir)
             f = np.load(os.path.join(path, 'frontal.npy'))
             l = np.load(os.path.join(path, 'lateral.npy'))
-            s = np.load(os.path.join(path, 'shape.npy'))
+
+            if self.predict_only:
+                s = []
+            else:
+                s = np.load(os.path.join(path, 'shape.npy'))
+            
 
             self.frontal.append(f)
             self.lateral.append(l)
@@ -44,6 +50,7 @@ class HSEDataset(Dataset):
         if self.transform:
             f = self.transform(f)
             l = self.transform(l)
+        
         return n, f, l, s
 
     def __len__(self):
